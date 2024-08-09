77f756583beed25540fecbc7f46cddca102902d4
MaryamRiaz-chattha <maryamriaz800@gmail.com>
2024-08-09 15:34:50 +0500

    navigation and linking Assignment with NextJs and Tailwind CSS

---
+++ src/app/homepage/page.tsx
@@ -0,0 +1,12 @@
+
+
+import React from 'react'
+
+function homepage() {
+  return (
+    <h1 className="font-bold text-6xl mx-2 my20 text-center">Welcome to NamePage</h1>
+    
+  )
+}
+
+export default homepage
---
+++ src/app/namepage/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function namepage() {
+  return (
+    
+    <h1 className="font-bold text-6xl mx-2 my20 text-center">Welcome to NamePage</h1>
+    
+  )
+}
+
+export default namepage
